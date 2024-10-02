c53869c9925c669487b0bf25ed964d3417bacf69
Juan Quintanar <juan.quintanar@koneksys.com>
2024-10-02 15:43:02 -0600

    feat(hello-world): add hello-world code
    Signed-off by: Juan Quintanar

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include the iostream library for input and output
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print the message to the console
+    return 0;  // Return 0 to indicate successful execution
+}
